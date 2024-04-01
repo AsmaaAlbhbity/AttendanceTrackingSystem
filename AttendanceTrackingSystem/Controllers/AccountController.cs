453b202b3dc847a4cf58671803dac2776341c28b
your_username <abdooaiman@std.mans.edu.eg>
2024-04-01 22:28:50 +0200

    View Pro

--- AttendanceTrackingSystem/Controllers/AccountController.cs
+++ AttendanceTrackingSystem/Controllers/AccountController.cs
@@ -33,7 +33,6 @@ namespace AttendanceTrackingSystem.Controllers
 				if (user != null)
 				{
 					Claim claim1 = new Claim(ClaimTypes.Name, user.Name);
-					Claim claim3 = new Claim(ClaimTypes.Email, user.Email);
 					Claim claim2;
 					if (user.UserType != "Employee")
 					{
@@ -43,13 +42,16 @@ namespace AttendanceTrackingSystem.Controllers
 					{
 						claim2 = new Claim(ClaimTypes.Role, repoAccount.GetEmployeeType(user.UserId));
 					}
-					//Claim claim3 = new Claim(ClaimTypes., user.Email);
+					Claim claim3 = new Claim(ClaimTypes.Email, user.Email);
+					Claim claim4 = new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString());
 					ClaimsIdentity identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
 					identity.AddClaim(claim1);
 					identity.AddClaim(claim2);
 					identity.AddClaim(claim3);
+					identity.AddClaim(claim4);
 					ClaimsPrincipal principal = new ClaimsPrincipal(identity);
 					await HttpContext.SignInAsync(principal);
+					TempData["role"] = claim2.Value;
 					ViewBag.img = user.ImgUrl ?? "~images/user.png";
 
 					return RedirectToAction("Index", "Home");
@@ -65,6 +67,23 @@ namespace AttendanceTrackingSystem.Controllers
 		{
 			await HttpContext.SignOutAsync();
 			return RedirectToAction("Login");
+		}
+		public IActionResult Profile()
+		{
+			int userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+			User model = repoAccount.GetUserByid(userId);
+			
+			if (model.ImgUrl == null)
+				ViewBag.img = "/images/user.png";
+			else
+				ViewBag.img = model.ImgUrl;
+
+			if (model.UserType == "Employee")
+				ViewBag.role = repoAccount.GetEmployeeType(userId).ToString();
+			else
+				ViewBag.role = model.UserType;
+			
+			return View(model);
 		}
 
 	}
--- AttendanceTrackingSystem/Repository/RepoAccount.cs
+++ AttendanceTrackingSystem/Repository/RepoAccount.cs
@@ -19,5 +19,9 @@ namespace AttendanceTrackingSystem.Repository
 		{
 			return db.Employees.FirstOrDefault(a => a.UserId == id).EmployeeType.ToString();
 		}
+		public User GetUserByid(int id)
+		{
+			return db.Users.FirstOrDefault(a => a.UserId == id);
+		}
 	}
 }
