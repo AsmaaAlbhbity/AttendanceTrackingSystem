db5294f63ebd4d89b70e1bcd45719136f733e32a
AsmaaAlbhbity <asmaa.albhbity@gmail.com>
2024-04-04 18:10:14 +0200

    update hamed

--- AttendanceTrackingSystem/Program.cs
+++ AttendanceTrackingSystem/Program.cs
@@ -32,7 +32,7 @@ namespace AttendanceTrackingSystem
             builder.Services.AddScoped<IRepoAccount, RepoAccount>();
 
            
-            var app = builder.Build();
+           
 
 
 
@@ -43,10 +43,10 @@ namespace AttendanceTrackingSystem
 					options.AccessDeniedPath = "/Home/AccessDenied";
 				});
 
-			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
+            var app = builder.Build();
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
 			{
 				app.UseExceptionHandler("/Home/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
